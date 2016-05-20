37efb7cd99ffe51c86ad0fb76c2229914e4306da
victoriasimonetti <victoriasimonetti@gmail.com>
2016-05-20 01:01:25 -0300

    adicionado nova activity

--- RangoVoice/app/src/main/java/isadora/rangovoice/MainActivity.java
+++ RangoVoice/app/src/main/java/isadora/rangovoice/MainActivity.java
@@ -1,23 +1,28 @@
 package isadora.rangovoice;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.Toast;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView list_view;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         list_view = (ListView) findViewById(R.id.list); // list é o nome do arquivo xml que define <ListView>
 
         // Defined Array values to show in ListView
-        String[] values = new String[] { "Bolo de chocolate", "Feijão tropeiro", "Bolinho de arroz", "Mousse de maracujá",
+        String[] values = new String[]{"Bolo de chocolate", "Feijão tropeiro", "Bolinho de arroz", "Mousse de maracujá",
                 "Massa à carbonara",
                 "Filé ao molho madeira",
                 "Pizza caseira",
@@ -59,21 +64,60 @@ public class MainActivity extends AppCompatActivity {
 
                 // ListView Clicked item index
                 int itemPosition     = position;
-
-                // ListView Clicked item value
-                String  itemValue    = (String) list_view.getItemAtPosition(position);
-
-                // Show Alert
-                Toast.makeText(getApplicationContext(),
-                        "Position :"+itemPosition+"  ListItem : " +itemValue , Toast.LENGTH_LONG)
-                        .show();
-
-                /* No nosso caso, devemos lançar uma segunda activity a partir desta - exibir a receita. Para isso, usamos um
-                * intent. Olhar site "como criar Activity". */
+                //Cria nova activity
+                Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
+                //Para passar por parâmetro o item selecionado:
+                Bundle b = new Bundle();
+                b.putInt("id", itemPosition); //Your id
+                secondActivity.putExtras(b); //Put your id to your next Intent
+                startActivity(secondActivity);
 
             }
 
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://isadora.rangovoice/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://isadora.rangovoice/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
